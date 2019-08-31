3a0389859616e37ea93a683428ce73ef144a7ad5
ferkki <ferkki@hotmail.com>
2019-08-31 08:22:14 -0400

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,23 @@
 // Puerto
 //=======================
 process.env.PORT = process.env.PORT || 3000;
+
+//=======================
+// Entorno
+//=======================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//=======================
+// base de datos
+//=======================
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+  urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+  urlDB =
+    'mongodb+srv://devfer:cuckoo28@cluster0-faqka.mongodb.net/test?retryWrites=true&w=majority';
+}
+
+process.env.URLDB = urlDB;
