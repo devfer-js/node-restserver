49ff9d50754759215823b967798a0931786a4136
ferkki <ferkki@hotmail.com>
2019-08-31 08:31:35 -0400

    variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -17,8 +17,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
   urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-  urlDB =
-    'mongodb+srv://devfer:cuckoo28@cluster0-faqka.mongodb.net/test?retryWrites=true&w=majority';
+  urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
